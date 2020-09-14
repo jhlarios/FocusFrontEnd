10b225b46a598c9c5e5119e9742f754f9f6f8306
hector larios <hlarios7@hotmail.com>
2020-09-14 00:40:53 -0600

    Confirm delete

--- src/app/components/confirm-delete-photo/confirm-delete-photo.component.ts
+++ src/app/components/confirm-delete-photo/confirm-delete-photo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../../../Services/rest.service';
 import { FormBuilder, FormGroup,FormControl, FormArray, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../../../Models/Photo';
 
 @Component({
   selector: 'app-confirm-delete-photo',
@@ -9,14 +11,34 @@ import { FormBuilder, FormGroup,FormControl, FormArray, Validators } from '@angu
 })
 export class ConfirmDeletePhotoComponent implements OnInit {
 
-  constructor(private rs : RestService,private formBuilder:FormBuilder) { }
- 
+  constructor(private route: ActivatedRoute,private rs : RestService,private formBuilder:FormBuilder) { }
+  id:string; 
+  columns = [ "Title"];
+  index = ["title"];
+  photos : Photo[] = [];
   Delete():void
   {
-    
+      this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      this.rs.deletePhotos(this.id).subscribe
+   (
+     (response)=>
+     {
+       this.photos = response;
+       
+     },
+     (error)=>
+     {
+       console.log("Error Occured : "+error);
+     }
+   )
+     
+   }); 
   }
 
   ngOnInit(): void {
+
+    
   }
 
 }
